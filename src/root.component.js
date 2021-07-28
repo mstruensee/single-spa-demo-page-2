945181c3f6753ebad81fac072e37693c6d553ccc
Matthew Struensee <mstruensee@gmail.com>
2021-07-28 13:45:27 -0500

    first commit

---
+++ src/root.component.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./root.component.css";
+export default function Root() {
+  return (
+    <div className="container2">
+      <p>Page 2 App</p>
+    </div>
+  );
+}
